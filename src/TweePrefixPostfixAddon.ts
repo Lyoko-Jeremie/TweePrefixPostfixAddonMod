f1b36bfb88d913d31f40fb2e4231a0714a0f06ae
LyokoJeremie <lucheng989898@gmail.com>
2023-12-03 04:51:02 +0800

    init

---
+++ src/TweePrefixPostfixAddon.ts
@@ -0,0 +1,20 @@
+import JSZip from "jszip";
+import type {LifeTimeCircleHook, LogWrapper} from "../../../dist-BeforeSC2/ModLoadController";
+import type {SC2DataManager} from "../../../dist-BeforeSC2/SC2DataManager";
+import type {ModUtils} from "../../../dist-BeforeSC2/Utils";
+import type {ModBootJson, ModInfo} from "../../../dist-BeforeSC2/ModLoader";
+import {isArray, isNil, isString} from 'lodash';
+
+export class TweePrefixPostfixAddon {
+    private logger: LogWrapper;
+
+    constructor(
+        public gSC2DataManager: SC2DataManager,
+        public gModUtils: ModUtils,
+    ) {
+        this.logger = gModUtils.getLogger();
+    }
+
+    init() {
+    }
+}
---
+++ src/init.ts
@@ -0,0 +1,4 @@
+import {TweePrefixPostfixAddon} from "./TweePrefixPostfixAddon";
+
+window.addonTweePrefixPostfixAddon = new TweePrefixPostfixAddon(window.modSC2DataManager, window.modUtils);
+window.addonTweePrefixPostfixAddon.init();
