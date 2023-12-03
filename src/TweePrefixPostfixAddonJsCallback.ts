9b5ab5fb7a1a290bf4bf81aca8b1b2691bf340ee
LyokoJeremie <lucheng989898@gmail.com>
2023-12-03 17:39:50 +0800

    readme

--- src/TweePrefixPostfixAddonJsCallback.ts
+++ src/TweePrefixPostfixAddonJsCallback.ts
@@ -15,12 +15,14 @@ export interface TweePrefixPostfixAddonCallback {
      * @param text
      * @param passageTitle
      * @param passageObj
+     * @return new passage inner text give the next callback OR as the final result to the wikify
      */
     beforePassage?: (text: string, passageTitle: string, passageObj: SC2Passage) => string;
     afterPassage?: (text: string, passageTitle: string, passageObj: SC2Passage) => void;
     /**
      * return new wikify text
      * @param text
+     * @return new wikify text give the next callback OR as the final result to the wikify
      */
     beforeWikify?: (text: string) => string;
     afterWikify?: (text: string) => void;
@@ -30,6 +32,7 @@ export interface TweePrefixPostfixAddonCallback {
      * @param widgetName
      * @param passageTitle
      * @param passageObj
+     * @return new widget inner text give the next callback OR as the final result to the wikify
      */
     beforeWidget?: (text: string, widgetName: string, passageTitle?: string, passageObj?: SC2Passage) => string;
     afterWidget?: (text: string, widgetName: string, passageTitle?: string, passageObj?: SC2Passage) => void;
